123bf8f9441655f95d2e3c8f729b7c0a2e17b89b
Krishanu <krishanudutta41@gmail.com>
2022-03-12 20:29:31 +0530

    added function headers

--- math.py
+++ math.py
@@ -1,9 +1,12 @@
 #add function implementation
 def add(x,y)
     pass
+#substract function implementation
 def substract(x,y)
     pass
+#multiply function implementation
 def multiply(x,y)
     pass
+#divide function implementation
 def divide(x,y)
     pass
